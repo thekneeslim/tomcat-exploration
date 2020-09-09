9ef6a5c447098f18576cbcbb4d8ccf20b544aaf1
Denise <denise.lim.sh@gmail.com>
2020-09-09 23:16:53 +0800

    Application listener to print out tomcat configuration

---
+++ application/src/main/java/org/example/configuration/TomcatThreadPoolListener.java
@@ -0,0 +1,22 @@
+package org.example.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatThreadPoolListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private ServerProperties serverProperties;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println(String.format("[TOMCAT CONFIG] Max Connections: %d", serverProperties.getTomcat().getMaxConnections()));
+        System.out.println(String.format("[TOMCAT CONFIG] Max Threads: %d", serverProperties.getTomcat().getMaxThreads()));
+        System.out.println(String.format("[TOMCAT CONFIG] Accept Count: %d", serverProperties.getTomcat().getAcceptCount()));
+    }
+}
+
